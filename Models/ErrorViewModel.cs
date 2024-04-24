495061ae1bfbf435814eb333d670bdc19d27bffd
Kamila <juliana.sosa@udla.edu.ec>
2024-04-24 10:34:52 -0500

    Creación del archivo

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace JulianaSosa_Examen1P.Models
 {
     public class ErrorViewModel
     {
+        //Creacion del archivo
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
