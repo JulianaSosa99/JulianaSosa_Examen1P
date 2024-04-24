960f72bca2da231ec97343b2485007ceb529551e
Kamila <juliana.sosa@udla.edu.ec>
2024-04-24 11:16:34 -0500

    Añadi Atributo cantidad con su propia DattaAnnotation

--- Models/JS_Medicinas.cs
+++ Models/JS_Medicinas.cs
@@ -8,12 +8,30 @@ namespace JulianaSosa_Examen1P.Models
         public int JS_MedicinasID {  get; set; }
         [MaxLength(20, ErrorMessage="Por favor ingresar hasta 20 caracteres"),MinLength(5)]
         public string? JS_DescripcionMedicina { get; set; }
+        [RequerirCantidad]
+        public int cantidad {  get; set; }
 
         [Required]
         public DateTime JS_FechaDeEntrega { get; set; }
          //Prueba
         public bool JS_ConReceta {  get; set; }
 
-        
+        public class RequerirCantidad : ValidationAttribute
+        {
+            public override bool IsValid(object? value)
+            {
+                decimal valor = (int)value;
+                if (valor <= 0)
+                {
+                    return false;
+
+                }
+                else
+                {
+                    return true;
+                }
+                //return base.IsValid(value);
+            }
+
+        }
     }
-}
