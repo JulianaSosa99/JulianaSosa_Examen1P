587c1c0a8c160a121c7e4e5b27b7d3f38a80a3ae
Kamila <juliana.sosa@udla.edu.ec>
2024-04-24 10:47:12 -0500

    Creacion de la Clase JS_Medicinas y creacion de varios atributos con 3 Datta anotation

---
+++ Models/JS_Medicinas.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JulianaSosa_Examen1P.Models
+{
+    public class JS_Medicinas
+    {
+        [Required]
+        public int JS_MedicinasID {  get; set; }
+        [MaxLength(20, ErrorMessage="Por favor ingresar hasta 20 caracteres"),MinLength(5)]
+        public string? JS_DescripcionMedicina { get; set; }
+
+        [Required]
+        public DateTime JS_FechaDeEntrega { get; set; }
+         
+        public bool JS_ConReceta {  get; set; }
+
+        
+    }
+}
